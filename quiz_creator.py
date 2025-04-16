128534f90a45704c091dd97fcfe6c7237d2daae3
hcrhannah <hannhacar@gmail.com>
2025-04-16 22:30:26 +0800

    initial commit, Basic Q&A Structure Input Prompts

---
+++ quiz_creator.py
@@ -0,0 +1,8 @@
+print("Enter your question (or type 'exit' to stop):")
+question = input("Enter question: ")
+a = input("Choice a: ")
+b = input("Choice b: ")
+c = input("Choice c: ")
+d = input("Choice d: ")
+answer = input("Correct answer (a/b/c/d): ")
+
